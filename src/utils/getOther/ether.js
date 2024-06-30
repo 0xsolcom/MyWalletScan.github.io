dc998541c0b1dbfc26657375111a4733ec0bebf8
xzh6688 <97216977+xzh6688@users.noreply.github.com>
2024-06-30 18:34:49 +0800

    Update ether.js

--- src/utils/getOther/ether.js
+++ src/utils/getOther/ether.js
@@ -10,8 +10,8 @@ const ether = async (address) => {
 
         // 检查数据是否为对象并且包含所需的字段
         if (data && typeof data === 'object') {
-            const totalIntegrationEigenLayerPoints = (Number(data.totalIntegrationEigenLayerPoints)).toFixed(2);
-            const totalIntegrationLoyaltyPoints = (Number(data.totalIntegrationLoyaltyPoints)).toFixed(2);
+            const totalIntegrationEigenLayerPoints = parseInt(Number(data.totalIntegrationEigenLayerPoints).10);
+            const totalIntegrationLoyaltyPoints = parseInt(Number(data.totalIntegrationLoyaltyPoints).10);
             return { totalIntegrationEigenLayerPoints, totalIntegrationLoyaltyPoints: totalIntegrationLoyaltyPoints };
         } else {
             // 如果数据不是对象或者不包含所需字段，返回默认值
