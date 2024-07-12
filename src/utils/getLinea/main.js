77da6197a28e5b78bf8f8ed78aa9524d9f40eb63
xzh6688 <97216977+xzh6688@users.noreply.github.com>
2024-07-12 16:09:52 +0800

    Update main.js

--- src/utils/getLinea/main.js
+++ src/utils/getLinea/main.js
@@ -1,26 +1,29 @@
-import getL1ToL2 from "./getL1ToL2.js";
+//import getL1ToL2 from "./getL1ToL2.js";
 import getBalance from "./getBalance.js";
 import getxp from "./getxp.js";
-import getTransactions from "./getTransactions.js";
-import getL2ToL1 from "./getL2ToL1.js";
-import getActivity from "./getActivity.js";
+import getracker from "./getracker.js";
+//import getTransactions from "./getTransactions.js";
+//import getL2ToL1 from "./getL2ToL1.js";
+//import getActivity from "./getActivity.js";
 
 const getLineaData = async (address) => {
     let data;
     try {
-        const transactions = await getTransactions(address);
+        //const transactions = await getTransactions(address);
         const balance = await getBalance(address);
         const xp = await getxp(address);
-        const L1ToL2 = await getL1ToL2(address);
-        const L2ToL1 = getL2ToL1(address, transactions)
-        const activity = getActivity(address, transactions)
+        const racker = await getracker(address);
+        //const L1ToL2 = await getL1ToL2(address);
+        //const L2ToL1 = getL2ToL1(address, transactions)
+        //const activity = getActivity(address, transactions)
         data = {
             address,
             balance,
             xp,
-            L1ToL2,
-            L2ToL1,
-            activity,
+            racker,
+            //L1ToL2,
+            //L2ToL1,
+            //activity,
             result: "success"
         }
         return data
