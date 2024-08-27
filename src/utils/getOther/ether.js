5abe885c59223fd3ecd2cc8db83905e18eff702b
xzh6688 <97216977+xzh6688@users.noreply.github.com>
2024-08-27 14:09:12 +0800

    Update ether.js
    Ether only displays the third quarter points

--- src/utils/getOther/ether.js
+++ src/utils/getOther/ether.js
@@ -10,8 +10,8 @@ const ether = async (address) => {
 
         // 检查数据是否为对象并且包含所需的字段
         if (data && typeof data === 'object') {
-            const totalIntegrationEigenLayerPoints = parseInt(Number(data.totalIntegrationEigenLayerPoints)).toFixed(0);
-            const totalIntegrationLoyaltyPoints = parseInt(Number(data.totalIntegrationLoyaltyPoints)).toFixed(0);
+            const totalIntegrationEigenLayerPoints = parseInt(Number(data.totalEigenLayerPoints)).toFixed(0);
+            const totalIntegrationLoyaltyPoints = parseInt(Number(data.s3Points)).toFixed(0);
             return { totalIntegrationEigenLayerPoints, totalIntegrationLoyaltyPoints: totalIntegrationLoyaltyPoints };
         } else {
             // 如果数据不是对象或者不包含所需字段，返回默认值
