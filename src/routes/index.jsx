d01e41d98a14e285baedbdd98dcee796c11545cf
僵小尸 <xiaoheshang_56@163.com>
2024-08-30 14:50:45 +0800

    更新 Coffee
    Signed-off-by: 僵小尸 <xiaoheshang_56@163.com>

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,76 +1,70 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 
-
-import {Navigate, useRoutes} from "react-router-dom";
-import StarkInfo from "@pages/StarkInfo/index.jsx";
+import Base from "@pages/Base/index.jsx";
 import Linea from "@pages/Linea/index.jsx";
 import Scroll from "@pages/Scroll/index.jsx";
-import Base from "@pages/Base/index.jsx";
+import { Navigate, useRoutes } from "react-router-dom";
 
 const Zksync = React.lazy(() => import("@pages/Zksync"));
 const MainPage = React.lazy(() => import("@pages/MainPage"));
 const Stark = React.lazy(() => import("@pages/Stark"));
 const Layer = React.lazy(() => import("@pages/Layer"));
-const Coffee = React.lazy(() => import("@pages/Coffee"));
+// const Coffee = React.lazy(() => import("@pages/Coffee"));
 const ZkInfo = React.lazy(() => import("@pages/ZkInfo"));
 const Setting = React.lazy(() => import("@pages/Setting"));
 const router = [
-    {
-        path: '/', element: <MainPage/>,
-        children: [
-            {
-                path: '/zksync',
-                element: <Zksync/>,
-            },
-            //{
-            //    path: '/zk_info',
-            //    element: <ZkInfo/>
-            //},
-            // {
-            //     path: '/stark',
-            //     element: <Stark/>,
-            // },
-            // {
-            //     path: '/stark_info',
-            //     element: <StarkInfo/>
-            // },
-            {
-                path: '/linea',
-                element: <Linea/>
-            },
-            {
-                path: '/Scroll',
-                element: <Scroll/>
-            },
-            {
-                path: '/Base',
-                element: <Base/>
-            },
-            // {
-            //     path: '/Layer',
-            //     element: <Layer/>,
-            // },
-            {
-                path: '/coffee',
-                element: <Coffee/>,
-            },
-            {
-                path: '/setting',
-                element: <Setting/>
-            }
-        ]
-    },
-    {path: "*", element: <Navigate to="/"/>},
-
+  {
+    path: "/",
+    element: <MainPage />,
+    children: [
+      {
+        path: "/zksync",
+        element: <Zksync />,
+      },
+      //{
+      //    path: '/zk_info',
+      //    element: <ZkInfo/>
+      //},
+      // {
+      //     path: '/stark',
+      //     element: <Stark/>,
+      // },
+      // {
+      //     path: '/stark_info',
+      //     element: <StarkInfo/>
+      // },
+      {
+        path: "/linea",
+        element: <Linea />,
+      },
+      {
+        path: "/Scroll",
+        element: <Scroll />,
+      },
+      {
+        path: "/Base",
+        element: <Base />,
+      },
+      // {
+      //     path: '/Layer',
+      //     element: <Layer/>,
+      // },
+      // {
+      //     path: '/coffee',
+      //     element: <Coffee/>,
+      // },
+      {
+        path: "/setting",
+        element: <Setting />,
+      },
+    ],
+  },
+  { path: "*", element: <Navigate to="/" /> },
 ];
 
 const WrapperRouter = () => {
-    let result = useRoutes(router);
-    return (
-        <Suspense>
-            {result}
-        </Suspense>
-    );
+  let result = useRoutes(router);
+  return <Suspense>{result}</Suspense>;
 };
 
 export default WrapperRouter;
